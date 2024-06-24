0ffe1542ced6bbb3ab579b014f826aa382fecfdc
Claus Wilke <wilke@austin.utexas.edu>
2024-06-23 22:05:42 -0500

    nearly complete workshop notebook

--- materials/trends_files/detrending_experiments.R
+++ materials/trends_files/detrending_experiments.R
@@ -3,47 +3,45 @@
 # weather station: AUSTIN CAMP MABRY, TX US USW00013958
 # temperature and precipitation data
 
-atx_temps <- read_delim(
-  "~/Desktop/USW00013958.csv"
-) |>
-  select(date = DATE, temp_ave = TAVG, temp_max = TMAX, temp_min = TMIN, precip = PRCP) |>
-  separate_wider_delim(date, delim = "-", names = c("year", "month")) |>
-  mutate(
-    year = as.numeric(year),
-    month = as.numeric(month),
-    date_dec = year + (month-1)/12
-  )
+library(tidyverse)
+library(here)
 
+atx_temps <- read_csv(
+  here("datasets", "atx_temps.csv"),
+  show_col_types = FALSE
+)
 
-start <- 2000
+start <- 1970
 end <- 2010
 temps_reduced <- filter(atx_temps, year >= start & year <= end)
+response <- temps_reduced$temp_ave
+response <- temps_reduced$precip
 
 
 # convert to time series object
-data <- temps_reduced$temp_ave
 temps_ts <- ts(
-  data = data,
+  data = response,
   start = start,
   end = c(end, 12),
   frequency = 12      # we have 12 time points per year
 )
 
-
 temps_stl <- stl(temps_ts, s.window = 7)
 
 temps_detrended <- tibble(
   date_dec = temps_reduced$date_dec,
-  data,
+  response,
   seasonal = t(temps_stl$time.series)[1, ],
   trend = t(temps_stl$time.series)[2, ],
   remainder = t(temps_stl$time.series)[3, ]
 )
 
 # plot
-p1 <- ggplot(temps_detrended, aes(date_dec, data)) + geom_line()
-p2 <- ggplot(temps_detrended, aes(date_dec, seasonal)) + geom_line()
-p3 <- ggplot(temps_detrended, aes(date_dec, trend)) + geom_line()
-p4 <- ggplot(temps_detrended, aes(date_dec, remainder)) + geom_line()
-
-plot_grid(p1, p2, p3, p4, ncol = 1, align = 'v')
+temps_detrended |>
+  pivot_longer(-date_dec, names_to = "component", values_to = "temperature") |>
+  mutate(
+    component = fct_relevel(component, "response", "trend", "seasonal", "remainder")
+  ) |>
+  ggplot(aes(date_dec, temperature)) +
+  geom_line() +
+  facet_wrap(~component, scales = "free_y", ncol = 1)
